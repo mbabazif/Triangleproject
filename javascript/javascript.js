74a2c7944ea06a51ebd153ca2c1e60dca6906d0e
Florence <mbababziflorence81@gmail.com>
2018-12-14 14:44:59 +0200

    adding form

---
+++ javascript/javascript.js
@@ -0,0 +1,31 @@
+function triangle() {
+    var X = parseInt(document.sides.X.value);
+    var Y = parseInt(document.sides.Y.value);
+    var Z = parseInt(document.sides.Z.value);
+    
+
+  
+  var XY = parseInt(document.sides.X.value) + parseInt(document.sides.Y.value);
+    var XZ = parseInt(document.sides.X.value) + parseInt(document.sides.Z.value);
+    var YZ = parseInt(document.sides.Z.value) + parseInt(document.sides.Z.value);
+
+    if ((X === Z) && (X=== Y) && (Y===Z)){
+        window.alert(" equilateral triangle!");
+    }
+    
+    else if ((X===Y) ||(X===Z) || (Y===Z)) {
+     window.alert(" Isoscelenes triangle!");
+   
+   }
+
+  else if ((XY > Z) && (XZ > Y) && (YZ > X) && (X!=Y) && (X!=Z) && (Y!=Z)) {
+   window.alert(" scalene triangle!");}
+   
+   else if ((XY <=Z) ||(XZ<= Y) ||(YZ <= X) ) {
+      window.alert("can not form a triangle!");}
+    else {
+        window.alert("This not valid number.");
+    }
+    
+            
+};
