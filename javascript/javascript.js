aa3a60e4215960d7d5abea319bdf3dc3de2cd6ae
Florence <mbababziflorence81@gmail.com>
2018-12-17 11:51:00 +0200

    adding  bdd

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,46 +1,38 @@
 function triangle() {
-    var X = parseInt(document.sides.X.value);
-    var Y = parseInt(document.sides.Y.value);
-    var Z = parseInt(document.sides.Z.value);
-    
+  var X = parseInt(document.sides.X.value);
+  var Y = parseInt(document.sides.Y.value);
+  var Z = parseInt(document.sides.Z.value);
 
-  
   var XY = parseInt(document.sides.X.value) + parseInt(document.sides.Y.value);
-    var XZ = parseInt(document.sides.X.value) + parseInt(document.sides.Z.value);
-    var YZ = parseInt(document.sides.Z.value) + parseInt(document.sides.Z.value);
+  var XZ = parseInt(document.sides.X.value) + parseInt(document.sides.Z.value);
+  var YZ = parseInt(document.sides.Z.value) + parseInt(document.sides.Z.value);
 
-    var number = 0;
-    var number1 = 0;
-    var number2 = 0;
+  var number = 0;
+  var number1 = 0;
+  var number2 = 0;
 
-    // var array_of_functions = [
-    //     first_function,
-    //     second_function,
-    //     third_function,
-    //     forth_function
-    // ]
-    // array_of_functions[0]('a string');
-    // if ( number ===number2)  (number1 === number2) && (number2 === number1)){
+  // var array_of_functions = [
+  //     first_function,
+  //     second_function,
+  //     third_function,
+  //     forth_function
+  // ]
+  // array_of_functions[0]('a string');
+  // if ( number ===number2)  (number1 === number2) && (number2 === number1)){
 
-    // }
-
-     if  ((X === Z) && (X=== Y) && (Y===Z)){
-        window.alert(" equilateral triangle!");
-    }
-    
-    else if ((X===Y) ||(X===Z) || (Y===Z)) {
-     window.alert(" Isoscelenes triangle!");
-   
-   }
-
-  else if ((XY > Z) && (XZ > Y) && (YZ > X) && (X!=Y) && (X!=Z) && (Y!=Z)) {
-   window.alert(" scalene triangle!");}
-   
-   else if ((XY <=Z) ||(XZ<= Y) ||(YZ <= X) ) {
-      window.alert("can not form a triangle!");}
-    else {
-        window.alert("This not valid number.");
-    }
-    
-            
-};
+  // }
+  //
+  if (X <= 0 || Y <= 0 || Z <= 0) {
+    window.alert("not a trianfle");
+  } else if (X === Z && X === Y && Y === Z) {
+    window.alert(" equilateral triangle!");
+  } else if (X === Y || X === Z || Y === Z) {
+    window.alert(" Isoscelenes triangle!");
+  } else if (XY > Z && XZ > Y && YZ > X && X != Y && X != Z && Y != Z) {
+    window.alert(" scalene triangle!");
+  } else if (XY <= Z || XZ <= Y || YZ <= X) {
+    window.alert("can not form a triangle!");
+  } else {
+    window.alert("This not valid number.");
+  }
+}
